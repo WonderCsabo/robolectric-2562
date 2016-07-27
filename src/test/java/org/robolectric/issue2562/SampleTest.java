cfe31692a929233959385213e56dab6bb78a4226
Csaba Kozak <kozakcsabi@gmail.com>
2016-07-27 09:59:57 +0200

    Initial commit

---
+++ src/test/java/org/robolectric/issue2562/SampleTest.java
@@ -0,0 +1,16 @@
+package org.robolectric.issue2562;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(manifest = "src/main/AndroidManifest.xml")
+@RunWith(RobolectricTestRunner.class)
+public class SampleTest {
+
+	@Test
+	public void test() {
+
+	}
+}
